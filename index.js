c903f5525ae300452046cbad4f6f760ce223b3b3
DeivisStasuls <deivis.stasuls@gmail.com>
2025-11-14 09:26:40 +0200

    tresais

--- index.js
+++ index.js
@@ -3,19 +3,16 @@ button = document.getElementById("guess");
 elerror = document.getElementById("elementerror");
 
 function myFunction (min, max) {
-    const inputfield = Math.floor(min);
-    const button = Math.floor(max);
-    return Math.floor(Math.random() * inputfield) + button;
+    const minimum = Math.floor(min);
+    const maximum = Math.floor(max);
+    return Math.floor(Math.random() * minimum) + maximum;
 }
 
-
-let value = addEventListener;
-
 button.addEventListener("click", function() {
-    button.getElementById(button).innerHTML = "elerror";
+    button.getElementById(button).innerHTML = myFunction(0,100);
     message.innerHTML = "";
     try{
-        if(button.value == "") throw 
+        if(button.value == "") throw "empty";
         if(isNaN(button.value)) throw "Please enter a valid number between 1 and 100";
     } catch(err) {
         message.innerHTML = "Error";
